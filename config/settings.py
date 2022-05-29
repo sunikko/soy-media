f1239394a0ed4eee95e286ff7663d65dc8eaf742
sunikko <sunikko77@gmail.com>
2022-05-29 15:16:30 +0100

    csrf error:

--- config/settings.py
+++ config/settings.py
@@ -29,10 +29,10 @@ environ.Env.read_env(
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['soytech.cf','*.soytech.cf']
 
 # Application definition
 
